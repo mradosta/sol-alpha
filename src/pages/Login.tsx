befc554f0b478bb37420ab4e8d5940dd3a08fadf
Martin Radosta <mradosta@pragmatia.com>
2024-10-31 16:11:10 -0300

    solution for the test

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -18,6 +18,10 @@ import { Grid } from '@material-ui/core';
 import {logoDiscord, logoTwitter,logoYoutube} from "ionicons/icons";
 import usePersistentState from '../hooks/usePersistentState';
 import meLogo from '../images/me.png';
+import Web3 from 'web3';
+
+
+
 
 /**
  * The "Login" page to which all unauthenticated users are redirected to
@@ -27,8 +31,43 @@ import meLogo from '../images/me.png';
 
 function Login() {
     const history = useHistory()
-	const dispatch = useDispatch();
+  	const dispatch = useDispatch();
     const user = useUser();
+    const [ buttonTittle, setButtonTittle ] = useState('Buy 1 NFT to gain access');
+
+
+    useEffect(() => {
+      // Connect to the BSC network (use a reliable BSC provider)
+      const web3 = new Web3('https://bsc-dataseed.binance.org/');
+
+      // Define the contract and the wallet address
+      const contractAddress = '0xb2ea51BAa12C461327d12A2069d47b30e680b69D';
+      const walletAddress = '0x248Dd3836E2A8B56279C04addC2D11F3c2497836';
+
+      // ABI with the balanceOf function
+      // https://bscscan.com/address/0xb2ea51BAa12C461327d12A2069d47b30e680b69D#readContract
+      const contractABI = [{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"spender","type":"address"},{"indexed":false,"internalType":"uint256","name":"value","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":true,"internalType":"address","name":"to","type":"address"},{"indexed":false,"internalType":"uint256","name":"value","type":"uint256"}],"name":"Transfer","type":"event"},{"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"address","name":"spender","type":"address"}],"name":"allowance","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"approve","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"account","type":"address"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"decimals","outputs":[{"internalType":"uint8","name":"","type":"uint8"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"subtractedValue","type":"uint256"}],"name":"decreaseAllowance","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"addedValue","type":"uint256"}],"name":"increaseAllowance","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"name","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"symbol","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"tSupply","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"totalSupply","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"recipient","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"transfer","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"sender","type":"address"},{"internalType":"address","name":"recipient","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"transferFrom","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"}];
+
+      // Create a contract instance
+      const contract = new web3.eth.Contract(contractABI, contractAddress);
+
+      // Function to fetch the balance
+      async function fetchBalance() {
+        try {
+            const balance = await contract.methods.balanceOf(walletAddress).call();
+            console.log('balance', balance);
+            setButtonTittle(balance + '');
+            return balance;
+        } catch (error) {
+            console.error('Error fetching balance:', error);
+            return null;
+        }
+      }
+      fetchBalance();
+    }, [] );
+
+
+
     const { nextUrl, urlCode, discordError } = useMemo(() => {
         const params = new URLSearchParams(window.location.search);
         // the login page also works as the redirect page for discord oauth
@@ -189,7 +228,7 @@ function Login() {
                                     </div>
                                     <IonButton className='buy-nft-btn mt-4 h-11'color='medium' onClick={()=> window.open('https://magiceden.io/marketplace/soldecoder', "_blank")}>
                                         <img src={meLogo} className="me-logo mr-2"/>
-                                        Buy 1 NFT to gain access
+                                        { buttonTittle }
                                     </IonButton>
                                     <IonButton className='buy-nft-btn mt-3 h-11' color='medium' onClick={()=> window.open('https://discord.gg/sol-decoder', "_blank")}>
                                         { <IonIcon icon={logoDiscord} className="big-emoji mr-2"/>}
